366c43a28c21d100a9c93c7fef43e4019a8307b0
Kenny Claka <kennyigbechi@gmail.com>
2019-05-26 15:44:36 -0700

    added change button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import React, {Component} from 'react';
 //import './App.css';
 
 export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username : "Kenny"
+        }
+    }
+
+    changeStateData = ()=>{
+        this.setState({
+            username : this.setState.username === "Kenny" ? "Claka" : "Eekne"
+        })
+}
   render(){
     return(
         <div>
           <h4 className="bg-primary text-white text-center p-2">
-            To Do List
+              {this.state.username}'s To Do List
           </h4>
+            <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+                Change
+            </button>
         </div>
     )
 
