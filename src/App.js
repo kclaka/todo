a7b88f913124ea2426ecd5606e8f735087a62eaf
Kenny Claka <kennyigbechi@gmail.com>
2019-05-26 16:54:51 -0700

    added input field

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ export default class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            username : "Kenny"
+            username : "Kenny",
+            todoItems : [{action:"Go to Ncore"},
+                         {action:"Get Quaters for laundry"},
+                         {action:"Pack my bags for my trip"},
+                         {action:"Get a Drink with Abel"}],
+            newItemText: ""
+        }
+    }
+
+    updateNewTextValue = (event) =>{
+        this.setState({newItemText : event.target.value});
+
+    }
+
+    createNewToDo = () =>{
+        if(this.state.todoItems
+            .find(item => item.action === this.state.newItemText)){
+            this.setState({
+                todoItems:[...this.state.todoItems, {action: this.state.newItemText, done: false}],
+                newItemText: ""
+            })
         }
     }
 
@@ -14,16 +34,23 @@ export default class App extends Component{
         this.setState({
             username : this.setState.username === "Kenny" ? "Claka" : "Eekne"
         })
-}
+    }
   render(){
     return(
         <div>
           <h4 className="bg-primary text-white text-center p-2">
               {this.state.username}'s To Do List
+              ({this.state.todoItems.filter(t => !t.done).length} items to do)
           </h4>
-            <button className="btn btn-primary m-2" onClick={this.changeStateData}>
-                Change
-            </button>
+            <div className="container-fluid">
+                <div className="my-1">
+                    <input className="form-control"
+                           value={this.state.newItemText}
+                           onChange={this.updateNewTextValue}/>
+                    <button className="btn btn-primary mt-1"
+                            onClick={this.createNewTodo}>Add</button>
+                </div>
+            </div>
         </div>
     )
 
